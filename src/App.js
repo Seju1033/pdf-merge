93071322df06567fe0b6f238e269226c6250c381
Seju <126666482+Seju1033@users.noreply.github.com>
2025-04-21 12:50:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,27 +3,27 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-
 import MergePDF from './pages/MergePDF';
 import CompressPDF from './pages/CompressPDF';
 import Footer from './components/Footer';
 import PDFViewer from './pages/PDFViewer';
-
-
-
-
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Hero />} />
-        <Route path="/merge-pdf" element={<MergePDF />} />
-        <Route path="/compress-pdf" element={<CompressPDF />} />
-        <Route path="/edit-pdf" element={<PDFViewer />} />
-      </Routes>
-      <Footer/>
+      <div className="app-layout">
+        <Navbar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Hero />} />
+            <Route path="/merge-pdf" element={<MergePDF />} />
+            <Route path="/compress-pdf" element={<CompressPDF />} />
+            <Route path="/edit-pdf" element={<PDFViewer />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </Router>
   );
 }
